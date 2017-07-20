99d08151d4ca2997b77b9166ab977c396b3e3d47
brett.yu <brett.yu@titansoft.com.sg>
2017-07-21 00:53:18 +0800

    passed the 5th test case "abc"

--- UnitTestProject/KataUnitTest.cs
+++ UnitTestProject/KataUnitTest.cs
@@ -13,8 +13,8 @@ namespace UnitTestProject
         [TestCase("abab", false, TestName = "Input_ab-ab_Should_Return_False")]
         [TestCase("aaaa", false, TestName = "Input_a-a-a-a_Should_Return_False")]
         [TestCase("x", true, TestName = "Input_x_Should_Return_True")]
-        /*[TestCase("abc", true, TestName = "Input_abc_Should_Return_True")]
-        [TestCase("fdsyffdsyffdsyffdsyffdsyf", false, TestName = "Input_fdsyf-fdsyf-fdsyf-fdsyf-fdsyf_Should_Return_False")]
+        [TestCase("abc", true, TestName = "Input_abc_Should_Return_True")]
+        /*[TestCase("fdsyffdsyffdsyffdsyffdsyf", false, TestName = "Input_fdsyf-fdsyf-fdsyf-fdsyf-fdsyf_Should_Return_False")]
         [TestCase("utdutdtdutd", true, TestName = "Input_utdutdtdutd_Should_Return_True")]
         [TestCase("abba", true, TestName = "Input_abba_Should_Return_True")]*/
         public void BasicTests(string input, bool expected)
