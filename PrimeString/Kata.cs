6bc8fbd240936de335386786e849928e7ecedab0
brett.yu <brett.yu@titansoft.com.sg>
2017-07-21 00:23:06 +0800

    first commit

---
+++ PrimeString/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeString
+{
+    public class Kata
+    {
+        public bool PrimeString(string s)
+        {
+            return true;
+        }
+    }
+}
---
+++ UnitTestProject/KataUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using PrimeString;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace UnitTestProject
+{
+    [TestFixture]
+    public class KataUnitTest
+    {
+
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new Kata();
+
+            Assert.AreEqual(true, kata.PrimeString("abac"));
+
+            Assert.AreEqual(false, kata.PrimeString("abab"));
+
+            Assert.AreEqual(false, kata.PrimeString("aaaa"));
+
+            Assert.AreEqual(true, kata.PrimeString("x"));
+
+            Assert.AreEqual(true, kata.PrimeString("abc"));
+
+            Assert.AreEqual(false, kata.PrimeString("fdsyffdsyffdsyffdsyffdsyf"));
+
+            Assert.AreEqual(true, kata.PrimeString("utdutdtdutd"));
+
+            Assert.AreEqual(true, kata.PrimeString("abba"));
+        }
+
+    }
+}
