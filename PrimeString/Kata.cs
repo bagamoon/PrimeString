b3069ee78c134ccc5992c9bc836ab120e6389e22
brett.yu <brett.yu@titansoft.com.sg>
2017-07-21 14:44:41 +0800

    refactor, remove unnecessary logic and rename variable

--- PrimeString/Kata.cs
+++ PrimeString/Kata.cs
@@ -13,13 +13,10 @@ namespace PrimeString
             if (s.Length <= 1)
                 return true;
 
-            if (s.Length == 2)
-                return s[0] != s[1];
-
             for (int i = 0; i <= s.Length / 2; i++)
             {
-                var count = s.Split(new string[] {s.Substring(0, i)}, StringSplitOptions.RemoveEmptyEntries).Length;
-                if (count == 0)
+                var splitCount = s.Split(new string[] {s.Substring(0, i)}, StringSplitOptions.RemoveEmptyEntries).Length;
+                if (splitCount == 0)
                     return false;
             }
 
